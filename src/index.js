c4750d47bcaad2a78ac2c28dc933e0a80861cadd
unknown <rushikesh.solunke@collabratechnology.com>
2024-06-20 21:49:01 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material/';
+import { ContextProvider } from './hooks/useStateContext';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  typography:{
+    fontFamily:'"IBM Plex Sans"'
+  }
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ContextProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
